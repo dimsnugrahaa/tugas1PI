aee3199c0a9ce2ad79b0c70717f8ae26600aa0f6
fikriacel12 <79891189+fikriacel12@users.noreply.github.com>
2021-04-05 23:39:45 +0700

    Add files via upload

---
+++ v_login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<style>
+h2 {
+  background-color: darkkhaki;
+}
+</style>
+</head>
+<body>
+	<!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url(); ?>template/backend/sbadmin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom Isi CSS -->
+    <link href="<?php echo base_url(); ?>template/backend/sbadmin/vendor/bootstrap/css/custom.css" rel="stylesheet">
+    <!-- Custom Login CSS -->
+    <link href="<?php echo base_url(); ?>template/backend/sbadmin/vendor/bootstrap/css/customlogin.css" rel="stylesheet"> 
+    <!-- MetisMenu CSS -->
+    <link href="<?php echo base_url(); ?>template/backend/sbadmin/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url(); ?>template/backend/sbadmin/dist/css/sb-admin-2.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="<?php echo base_url(); ?>template/backend/sbadmin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	
+	<center>
+		<h2 style="color: black;">WELCOME TO THE CLUB<br/></h2>
+	<center>
+	<form action="<?php echo base_url('login/aksi_login'); ?>" method="post">		
+		<div class="container">
+		<div class="row">
+    	<div class="col-md-3">
+    		</center>
+        <div class="panel panel-default" style=" background-color: black">
+            <div class="panel-heading" style="background-color: darkkhaki">
+                <strong>PLEASE LOGIN</strong>
+            </div>
+            <div class="panel-body"><p>
+				<div class="form-group">
+                    <p class="col-sm-3" style="color: darkkhaki"> Username </p>
+                    <p></p>
+					<div class="col-sm-8">
+						<input type="text" name="username" class="form-control" placeholder=" Username ">
+					</p></div>
+					
+				<div class="form-group">
+                    <p class="col-sm-3" style="color: darkkhaki"> Password </p>
+					<div class="col-sm-8">
+						<input type="password" name="password" class="form-control" placeholder=" Password ">
+					</div>
+				</div>
+			<div class="form-group last">
+                 <div class="col-sm-offset-3 col-sm-6">
+<h1></h1>
+                    <button type="submit" name="proses" class="btn btn-success btn-sm"> Sign In </button>
+                    <button type="reset" class="btn btn-default btn-sm"> Reset </button>
+                    <p></p>
+                    <button><a href="<?= base_url(); ?>register/"> Register </a></button>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
+	</form>
+	</center>
+	 <!-- jQuery -->
+	 <script src="<?php echo base_url(); ?>template/backend/sbadmin/vendor/jquery/jquery.min.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="<?php echo base_url(); ?>template/backend/sbadmin/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!-- Metis Menu Plugin JavaScript -->
+	<script src="<?php echo base_url(); ?>template/backend/sbadmin/vendor/metisMenu/metisMenu.min.js"></script>
+	<!-- Custom Theme JavaScript -->
+	<script src="<?php echo base_url(); ?>template/backend/sbadmin/dist/js/sb-admin-2.js"></script>
+</body>
+</html>
---
+++ v_user.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Login </title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+</head>
+<body style="background-color: darkkhaki">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div style="background-color: darkkhaki" class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <center><a class="nav-link" href="" style="color: black"> ACEL </a></center>
+                </li>
+            </ul>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="" style="color: black"> PALEK </a>
+                </li>
+            </ul>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="" style="color: black"> ZEED </a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a style="color: black" class="nav-link" href="<?php echo base_url('login/logout'); ?>"> Logout </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body" align="center">
+                    WELCOME TO THE CLUB AND ENJOY THIS HOMEPAGE.
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+</body>
+</html> 
